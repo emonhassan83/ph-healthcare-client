c58ce551a4c8e0e1ba54e823f2f6344c208ff448
Emon <emonhasan7650@gmail.com>
2024-05-07 13:19:13 +0600

    resuable modal and PHFile uploader resuable component done

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+
+import { Box, Button, Stack, TextField } from "@mui/material";
+import SpecialistModal from "./components/SpecialistModal";
+import { useState } from "react";
+
 const SpecialtiesPage = () => {
-    return (
-        <div>
-            <h1>Specialties Page </h1>
-        </div>
-    );
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  return (
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Button onClick={() => setIsModalOpen(true)}>Create Specialty</Button>
+        <SpecialistModal open={isModalOpen} setOpen={setIsModalOpen} />
+        <TextField size="small" placeholder="Search Specialists" />
+      </Stack>
+    </Box>
+  );
 };
 
 export default SpecialtiesPage;
