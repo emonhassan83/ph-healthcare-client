fbdc6709c725e06f8f3786ebbb32fab289547860
Emon <emonhasan7650@gmail.com>
2024-05-08 21:49:57 +0600

    ph-full screen modal and create doctor modal created

--- src/app/(withDashboardLayout)/dashboard/admin/doctors/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/doctors/page.tsx
@@ -1,9 +1,14 @@
+import { Box, Button, Stack, TextField } from "@mui/material";
+
 const DoctorsPage = () => {
-    return (
-      <div>
-        <h1>Doctors Page</h1>
-      </div>
-    );
-  };
-  
-  export default DoctorsPage;
+  return (
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Button>Create New Doctor</Button>
+        <TextField size="small" placeholder="Search doctor" />
+      </Stack>
+    </Box>
+  );
+};
+
+export default DoctorsPage;
